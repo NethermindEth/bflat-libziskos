33eb3a28d6b886bcbe50f3ed241d93e246017832
Maxim Menshikov <maksim.menshikov@nethermind.io>
2026-03-08 23:56:19 +0000

    Update C# bindings
    (Accurate rewrite of Ruben's 1e84e401f6a998691d8d49644562b146de771e92)

--- src/bindings/Crypto.cs
+++ src/bindings/Crypto.cs
@@ -1,7 +1,6 @@
 // SPDX-FileCopyrightText: 2026 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 namespace Nethermind.ZiskBindings;
@@ -14,97 +13,146 @@ public static unsafe partial class Crypto
     [UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
     public static partial void blake2b_compress_c(
         uint rounds,
-        ulong* state,
-        ulong* message,
-        ulong* offset,
+        Span<ulong> state,
+        ReadOnlySpan<ulong> message,
+        ReadOnlySpan<ulong> offset,
         byte final_block
     );
 
     [LibraryImport(NativeLibraryName, EntryPoint = "bls12_381_fp_to_g1_c")]
     [UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
-    public static partial byte bls12_381_fp_to_g1_c(byte* ret, byte* fp);
+    public static partial byte bls12_381_fp_to_g1_c(
+        Span<byte> ret,
+        ReadOnlySpan<byte> fp
+    );
 
     [LibraryImport(NativeLibraryName, EntryPoint = "bls12_381_fp2_to_g2_c")]
     [UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
-    public static partial byte bls12_381_fp2_to_g2_c(byte* ret, byte* fp2);
+    public static partial byte bls12_381_fp2_to_g2_c(
+        Span<byte> ret,
+        ReadOnlySpan<byte> fp2
+    );
 
     [LibraryImport(NativeLibraryName, EntryPoint = "bls12_381_g1_add_c")]
     [UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
-    public static partial byte bls12_381_g1_add_c(byte* ret, byte* a, byte* b);
+    public static partial byte bls12_381_g1_add_c(
+        Span<byte> ret,
+        ReadOnlySpan<byte> a,
+        ReadOnlySpan<byte> b
+    );
 
     [LibraryImport(NativeLibraryName, EntryPoint = "bls12_381_g1_msm_c")]
     [UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
-    public static partial byte bls12_381_g1_msm_c(byte* ret, byte* pairs, nuint num_pairs);
+    public static partial byte bls12_381_g1_msm_c(
+        Span<byte> ret,
+        ReadOnlySpan<byte> pairs,
+        nuint num_pairs
+    );
 
     [LibraryImport(NativeLibraryName, EntryPoint = "bls12_381_g2_add_c")]
     [UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
-    public static partial byte bls12_381_g2_add_c(byte* ret, byte* a, byte* b);
+    public static partial byte bls12_381_g2_add_c(
+        Span<byte> ret,
+        ReadOnlySpan<byte> a,
+        ReadOnlySpan<byte> b
+    );
 
     [LibraryImport(NativeLibraryName, EntryPoint = "bls12_381_g2_msm_c")]
     [UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
-    public static partial byte bls12_381_g2_msm_c(byte* ret, byte* pairs, nuint num_pairs);
+    public static partial byte bls12_381_g2_msm_c(
+        Span<byte> ret,
+        ReadOnlySpan<byte> pairs,
+        nuint num_pairs
+    );
 
     [LibraryImport(NativeLibraryName, EntryPoint = "bls12_381_pairing_check_c")]
     [UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
-    public static partial byte bls12_381_pairing_check_c(byte* pairs, nuint num_pairs);
+    public static partial byte bls12_381_pairing_check_c(
+        ReadOnlySpan<byte> pairs,
+        nuint num_pairs
+    );
 
     [LibraryImport(NativeLibraryName, EntryPoint = "bn254_g1_add_c")]
     [UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
-    public static partial byte bn254_g1_add_c(byte* p1, byte* p2, byte* ret);
+    public static partial byte bn254_g1_add_c(
+        ReadOnlySpan<byte> p1,
+        ReadOnlySpan<byte> p2,
+        Span<byte> ret
+    );
 
     [LibraryImport(NativeLibraryName, EntryPoint = "bn254_g1_mul_c")]
     [UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
-    public static partial byte bn254_g1_mul_c(byte* point, byte* scalar, byte* ret);
+    public static partial byte bn254_g1_mul_c(
+        ReadOnlySpan<byte> point,
+        ReadOnlySpan<byte> scalar,
+        Span<byte> ret
+    );
 
     [LibraryImport(NativeLibraryName, EntryPoint = "bn254_pairing_check_c")]
     [UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
-    public static partial byte bn254_pairing_check_c(byte* pairs, nuint num_pairs);
+    public static partial byte bn254_pairing_check_c(
+        ReadOnlySpan<byte> pairs,
+        nuint num_pairs
+    );
 
     [LibraryImport(NativeLibraryName, EntryPoint = "keccak256_c")]
     [UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
-    public static partial void keccak256_c(byte* input, nuint input_len, byte* output);
+    public static partial void keccak256_c(
+        ReadOnlySpan<byte> input,
+        nuint input_len,
+        Span<byte> output
+    );
 
     [LibraryImport(NativeLibraryName, EntryPoint = "modexp_bytes_c")]
     [UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
     public static partial nuint modexp_bytes_c(
-        byte* base_ptr,
+        ReadOnlySpan<byte> base_ptr,
         nuint base_len,
-        byte* exp_ptr,
+        ReadOnlySpan<byte> exp_ptr,
         nuint exp_len,
-        byte* modulus_ptr,
+        ReadOnlySpan<byte> modulus_ptr,
         nuint modulus_len,
-        byte* result_ptr
+        Span<byte> result_ptr
     );
 
     [LibraryImport(NativeLibraryName, EntryPoint = "secp256k1_ecdsa_address_recover_c")]
     [UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
     public static partial byte secp256k1_ecdsa_address_recover_c(
-        byte* sig,
+        ReadOnlySpan<byte> sig,
         byte recid,
-        byte* msg,
-        byte* output
+        ReadOnlySpan<byte> msg,
+        Span<byte> output
     );
 
     [LibraryImport(NativeLibraryName, EntryPoint = "secp256k1_ecdsa_verify_and_address_recover_c")]
     [UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
     public static partial byte secp256k1_ecdsa_verify_and_address_recover_c(
-        byte* sig,
-        byte* msg,
-        byte* pk,
-        byte* output
+        ReadOnlySpan<byte> sig,
+        ReadOnlySpan<byte> msg,
+        ReadOnlySpan<byte> pk,
+        Span<byte> output
     );
 
     [LibraryImport(NativeLibraryName, EntryPoint = "secp256r1_ecdsa_verify_c")]
     [UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
     [return: MarshalAs(UnmanagedType.U1)]
-    public static partial bool secp256r1_ecdsa_verify_c(byte* msg, byte* sig, byte* pk);
+    public static partial bool secp256r1_ecdsa_verify_c(
+        ReadOnlySpan<byte> msg,
+        ReadOnlySpan<byte> sig,
+        ReadOnlySpan<byte> pk
+    );
 
     [LibraryImport(NativeLibraryName, EntryPoint = "sha256_c")]
     [UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
-    public static partial void sha256_c(byte* input, nuint input_len, byte* output);
+    public static partial void sha256_c(ReadOnlySpan<byte> input, nuint input_len, Span<byte> output);
 
     [LibraryImport(NativeLibraryName, EntryPoint = "verify_kzg_proof_c")]
     [UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
     [return: MarshalAs(UnmanagedType.U1)]
-    public static partial bool verify_kzg_proof_c(byte* z, byte* y, byte* commitment, byte* proof);
+    public static partial bool verify_kzg_proof_c(
+        ReadOnlySpan<byte> z,
+        ReadOnlySpan<byte> y,
+        ReadOnlySpan<byte> commitment,
+        ReadOnlySpan<byte> proof
+    );
 }
