32f1f73bd1501be72018fb3b25ce5aa06d0bc31e
Ruben Buniatyan <rubo@users.noreply.github.com>
2026-03-13 20:22:09 +0100

    Implement new input read

--- src/bindings/IO.cs
+++ src/bindings/IO.cs
@@ -1,18 +1,19 @@
 // SPDX-FileCopyrightText: 2026 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
-using System;
 using System.Runtime.CompilerServices;
 
 namespace Nethermind.ZiskBindings;
 
 public static unsafe class IO
 {
-    private static readonly byte* Input = (byte*)0x9000_0000UL; // INPUT_ADDR
+    private static readonly byte* Input = (byte*)0x4000_0000UL; // INPUT_ADDR
     private static readonly uint* Output = (uint*)0xa001_0000UL; // OUTPUT_ADDR
     private static readonly byte* Uart = (byte*)0xa000_0200UL; // UART_ADDR
 
-    public static ReadOnlySpan<byte> ReadInput()
+    private static ulong _inputPosition = sizeof(ulong); // zkVM offset
+
+    public static ReadOnlySpan<byte> ReadInputLegacy()
     {
         ulong size = *(ulong*)(Input + sizeof(ulong));
 
@@ -21,6 +22,25 @@ public static unsafe class IO
 
         return new ReadOnlySpan<byte>(Input + 2 * sizeof(ulong), (int)size);
     }
+
+    public static ReadOnlySpan<byte> ReadInput()
+    {
+        byte* data = Input + checked((nint)_inputPosition); // [len: u64][data][padding]
+        ulong len = *(ulong*)data;
+
+        if (len > int.MaxValue)
+            Environment.FailFast("Input size exceeds the maximum supported length");
+
+        ulong alignedLen = (len + 7UL) & ~7UL;
+
+        // Advance by padded length
+        _inputPosition = checked(_inputPosition + sizeof(ulong) + alignedLen);
+
+        // Return the length of actual data, not including the padding
+        return new ReadOnlySpan<byte>(data + sizeof(ulong), (int)len);
+    }
+
+    public static void ReadReset() => _inputPosition = sizeof(ulong);
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void SetOutput(int id, uint value)
@@ -37,6 +57,9 @@ public static unsafe class IO
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void Write(string value)
     {
+        if (value is null)
+            return;
+
         for (int i = 0; i < value.Length; i++)
             *Uart = unchecked((byte)value[i]);
     }
